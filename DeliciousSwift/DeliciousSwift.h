a3c28e97a32439341e566339b920b60fe60a532e
drkameleon <drkameleon@gmail.com>
2017-11-07 13:20:52 +0100

    initial commit

---
+++ DeliciousSwift/DeliciousSwift.h
@@ -0,0 +1,19 @@
+//
+//  DeliciousSwift.h
+//  DeliciousSwift
+//
+//  Created by drkameleon on 11/7/17.
+//  Copyright © 2017 drkameleon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DeliciousSwift.
+FOUNDATION_EXPORT double DeliciousSwiftVersionNumber;
+
+//! Project version string for DeliciousSwift.
+FOUNDATION_EXPORT const unsigned char DeliciousSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeliciousSwift/PublicHeader.h>
+
+
