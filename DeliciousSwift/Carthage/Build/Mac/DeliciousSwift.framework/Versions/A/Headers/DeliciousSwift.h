649ca82a3785a29e0f9bfe69491e79aa36de902b
drkameleon <drkameleon@gmail.com>
2017-11-08 14:12:16 +0100

    setup for Carthage

---
+++ DeliciousSwift/Carthage/Build/Mac/DeliciousSwift.framework/Versions/A/Headers/DeliciousSwift.h
@@ -0,0 +1,24 @@
+/********************************************************
+ | DeliciousSwift
+ ********************************************************
+ | Copyright (c) 2017 InSili.co.
+ | All rights reserved.
+ |
+ | @File   : DeliciousSwift.h
+ | @Author : Dr.Kameleon
+ | @Date   : 11/07/17
+ --------------------------------------------------------
+ | Objective-C Bridge Header
+ ********************************************************/
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DeliciousSwift.
+FOUNDATION_EXPORT double DeliciousSwiftVersionNumber;
+
+//! Project version string for DeliciousSwift.
+FOUNDATION_EXPORT const unsigned char DeliciousSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeliciousSwift/PublicHeader.h>
+
+
