668e40682d5ea1b34f62be761b83e4654b3f1849
drkameleon <drkameleon@gmail.com>
2017-11-08 11:10:26 +0100

    added Booleans.swift

---
+++ DeliciousSwift/DeliciousSwift/Booleans.swift
@@ -0,0 +1,25 @@
+/********************************************************
+ | DeliciousSwift
+ ********************************************************
+ | Copyright (c) 2017 InSili.co.
+ | All rights reserved.
+ |
+ | @File   : Booleans.swift
+ | @Author : Dr.Kameleon
+ | @Date   : 11/07/17
+ --------------------------------------------------------
+ | Bool-related methods
+ ********************************************************/
+
+import Foundation
+
+public extension Bool {
+ 
+    /**
+     Negates/Toggles boolean value
+     */
+    func negate() -> Bool {
+        return !self
+    }
+    
+}
